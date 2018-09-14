1dee2bc8098b6b0ee0b940bda5852703850235e0
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-09-14 19:08:34 +0200

    Import sprintc() & printc() interface

---
+++ lib/printc.h
@@ -0,0 +1,30 @@
+#ifndef PRINTC_H
+#define PRINTC_H
+
+#include <stddef.h>
+
+void sprintc(const char *str, size_t len_ctr, char *res);
+
+  /* String Print Centre
+   * -------------------
+   * Centres string [str] in a space [len_ctr] characters wide and writes it to the address
+   * pointed to by [res]. It must have at least [len_ctr] + 1 bytes allocated.
+   *
+   * Example usage:
+   *
+   *   char *res = malloc((len_ctr + 1) * sizeof(*res));
+   *   sprintc("centre me", len_ctr, res);
+   *   [...]
+   *   free(res);
+   *
+   * ------------------- */
+
+void printc(const char *str, size_t len);
+
+  /* Print Centre
+   * ------------
+   * Wrapper function for sprintf.
+   * Prints output directly to stdout.
+   * ------------ */
+
+#endif
