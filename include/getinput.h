626dc7525c286d8b14e8dce9c83f3ac711d0ea51
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-09-17 18:28:52 +0200

    Import get_input() header

---
+++ include/getinput.h
@@ -0,0 +1,16 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+void get_input(int argc, char **argv, char *str);
+
+  /* Get Input From All Sources
+   * --------------------------
+   * Consolidates input from command line arguments,
+   * pipe, redirection and here string into [str]
+   *
+   * The pointer passed to [str] must
+   * have enough memory allocated.
+   * --------------------------
+   */
+
+#endif
