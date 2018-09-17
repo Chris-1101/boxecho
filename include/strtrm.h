052d2dc5e941afc1428e8bf898e3c91177f234d0
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-09-17 18:29:11 +0200

    Import strtrm() header

---
+++ include/strtrm.h
@@ -0,0 +1,16 @@
+#ifndef TRIMCHAR_H
+#define TRIMCHAR_H
+
+char* strtrm(char *str, const char chr);
+
+  /* Trim Character From String
+   * --------------------------
+   * Trims the character specified in [chr]
+   * from the beginning and end of [str].
+   *
+   * Returns a pointer to a null-terminated substring of the
+   * original. Must be able to write to the original string.
+   * --------------------------
+   */
+
+#endif
