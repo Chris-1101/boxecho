fb1f82cfd6c4a3c6c5794665076007b4bbe99cdb
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-09-17 18:30:15 +0200

    Import strtrm() implementation

---
+++ src/strtrm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "strtrm.h"
+
+// Trim Character From String Start/End
+char* strtrm(char *str, const char chr)
+{
+  if (str == NULL)
+  {
+    return NULL;
+  }
+
+  size_t len_str = strlen(str);
+
+  while(len_str > 0 && str[0] == chr)
+  {
+    str++;
+    len_str--;
+  }
+
+  while(len_str > 0 && str[len_str - 1] == chr)
+  {
+    str[--len_str] = '\0';
+  }
+
+  return str;
+}
